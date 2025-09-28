f6e39da1795c514f7fc0375d802aab22859a5b3d
imauice <102501547+imauice@users.noreply.github.com>
2025-09-28 12:17:06 +0700

    modified:   src/components/DashboardCard.vue

--- src/components/DashboardCard.vue
+++ src/components/DashboardCard.vue
@@ -5,8 +5,8 @@
         <div class="surface-card shadow-2 p-3 border-round">
           <div class="flex justify-content-between mb-3">
             <div>
-              <span class="block text-500 font-medium mb-3">Orders</span>
-              <div class="text-900 font-medium text-xl">152</div>
+              <span class="block text-500 font-medium mb-3">Rooms</span>
+              <div class="text-900 font-medium text-xl">{{ status.room }}</div>
             </div>
             <div
               class="flex align-items-center justify-content-center bg-blue-100 border-round"
@@ -15,16 +15,16 @@
               <i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
             </div>
           </div>
-          <span class="text-green-500 font-medium">24 new </span>
-          <span class="text-500">since last visit</span>
+          <!-- <span class="text-green-500 font-medium">24 new </span>
+          <span class="text-500">since last visit</span> -->
         </div>
       </div>
       <div class="col-12 md:col-6 lg:col-3">
         <div class="surface-card shadow-2 p-3 border-round">
           <div class="flex justify-content-between mb-3">
             <div>
-              <span class="block text-500 font-medium mb-3">Revenue</span>
-              <div class="text-900 font-medium text-xl">$2.100</div>
+              <span class="block text-500 font-medium mb-3">Bookings</span>
+              <div class="text-900 font-medium text-xl">{{ status.booking }}</div>
             </div>
             <div
               class="flex align-items-center justify-content-center bg-orange-100 border-round"
@@ -33,16 +33,16 @@
               <i class="pi pi-map-marker text-orange-500 text-xl"></i>
             </div>
           </div>
-          <span class="text-green-500 font-medium">%52+ </span>
-          <span class="text-500">since last week</span>
+          <!-- <span class="text-green-500 font-medium">%52+ </span>
+          <span class="text-500">since last week</span> -->
         </div>
       </div>
       <div class="col-12 md:col-6 lg:col-3">
         <div class="surface-card shadow-2 p-3 border-round">
           <div class="flex justify-content-between mb-3">
             <div>
-              <span class="block text-500 font-medium mb-3">Customers</span>
-              <div class="text-900 font-medium text-xl">28441</div>
+              <span class="block text-500 font-medium mb-3">Revenuse</span>
+              <div class="text-900 font-medium text-xl">{{ status.revenue }}</div>
             </div>
             <div
               class="flex align-items-center justify-content-center bg-cyan-100 border-round"
@@ -51,16 +51,16 @@
               <i class="pi pi-inbox text-cyan-500 text-xl"></i>
             </div>
           </div>
-          <span class="text-green-500 font-medium">520 </span>
-          <span class="text-500">newly registered</span>
+          <!-- <span class="text-green-500 font-medium">520 </span>
+          <span class="text-500">newly registered</span> -->
         </div>
       </div>
       <div class="col-12 md:col-6 lg:col-3">
         <div class="surface-card shadow-2 p-3 border-round">
           <div class="flex justify-content-between mb-3">
             <div>
-              <span class="block text-500 font-medium mb-3">Comments</span>
-              <div class="text-900 font-medium text-xl">152 Unread</div>
+              <span class="block text-500 font-medium mb-3">Customers</span>
+              <div class="text-900 font-medium text-xl">{{ status.customer }}</div>
             </div>
             <div
               class="flex align-items-center justify-content-center bg-purple-100 border-round"
@@ -76,3 +76,18 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      status:{
+        room:0,
+        booking:0,
+        revenue:0,
+        customer:0,
+
+      }
+    }
+  }
+}
+</script>
